43e3c6a9e5c9d2711eac0be116dcc4d9808515b3
Davide Zordan <info@davidezordan.net>
2019-01-07 21:21:52 +0000

    Adding initial project

---
+++ High-Immersion-RubeGoldberg-SteamVR2/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class LoadScene : MonoBehaviour {
+    public string SceneToLoad = "_Scenes/Level 1";
+
+    void Start (){
+        StartCoroutine(LoadSceneAsync());
+    }
+    
+    IEnumerator LoadSceneAsync() {
+        yield return new WaitForSeconds(4);
+               
+        SteamVR_LoadLevel.Begin(SceneToLoad);
+    }
+}
---
+++ High-Immersion-RubeGoldberg-SteamVR2/Assets/Scripts/ObjectMenuManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMenuManager : MonoBehaviour {
+    public List<GameObject> objectList; //handled automatically at start
+    public List<GameObject> objectPrefabList; //set manually in inspector and MUST match order
+                                              //of scene menu objects
+    public int currentObject = 0;                                       
+	// Use this for initialization
+	void Start () {
+		foreach(Transform child in transform)
+        {
+            objectList.Add(child.gameObject);
+        }
+	}
+
+    public void MenuLeft()
+    {
+        objectList[currentObject].SetActive(false);
+        currentObject--;
+        if(currentObject < 0)
+        {
+            currentObject = objectList.Count - 1;
+        }
+        objectList[currentObject].SetActive(true);
+    }
+    public void MenuRight()
+    {
+        objectList[currentObject].SetActive(false);
+        currentObject++;
+        if (currentObject > objectList.Count - 1)
+        {
+            currentObject = 0;
+        }
+        objectList[currentObject].SetActive(true);
+    }
+    public void SpawnCurrentObject()
+    {
+        Instantiate(objectPrefabList[currentObject], 
+            objectList[currentObject].transform.position, 
+            objectList[currentObject].transform.rotation);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
