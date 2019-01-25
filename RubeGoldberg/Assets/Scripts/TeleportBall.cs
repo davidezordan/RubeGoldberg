d9006f79b0ceb5ecc746e83f905975e195841eda
Davide Zordan <info@davidezordan.net>
2019-01-25 21:09:03 +0000

    Adding teleport points

---
+++ RubeGoldberg/Assets/Scripts/TeleportBall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportBall : MonoBehaviour {
+	private Rigidbody ballRigidbody;
+
+    private GameObject teleportOut;
+
+	void Start () {
+        var teleports = GameObject.FindGameObjectsWithTag("BallTeleportTargetOut");
+        if (teleports.Length > 0) {
+            teleportOut = teleports[0];
+        }
+	}
+
+    void OnTriggerEnter(Collider other) {
+        DebugManager.Info("TeleportBall.OnTriggerEnter(): " + other.tag);
+
+        if (teleportOut && other.CompareTag("BallTeleportTarget")) {
+            gameObject.SetActive(false);
+            gameObject.transform.position = teleportOut.transform.position;
+            gameObject.SetActive(true);            
+        }
+    }
+}
